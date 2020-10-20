ea25c4d9863616066cffdba86d927a3ec885da6a
Rahul <pareek.rahul199@gmail.com>
2020-10-21 00:14:28 +0530

    add

--- gold_app/urls.py
+++ gold_app/urls.py
@@ -1,19 +1,11 @@
-from django.contrib import admin
+from . import views
 from django.urls import path, include
-from gold_app.views import *
-from django.contrib.auth.views import LogoutView
+from gold_app.views import User
+
 
 app_name = 'gold_app'
 
 
 urlpatterns = [
-    # path('register', register),
-    # path('', user_login, name= "home"),
-    # path('conapp', user_login, name='conapp'),
-    # path('add-data', addData),
-    # path('trans-data',transData),
-    path('show/', show),
-    # path('logout',logout_view , name = "logout"),
-    # path('logout', logout_view, name='logout'),
-
+    path('show/', views.show),
 ]
