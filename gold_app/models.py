1ffdaf7bf406711bdb428a812a0d4d24318604ea
Rahul <pareek.rahul199@gmail.com>
2020-10-24 00:59:57 +0530

    changes in migrations

--- gold_app/models.py
+++ gold_app/models.py
@@ -34,16 +34,50 @@ class center(models.Model):
     # center_status = MultiSelectField(max_length = 12, choices = CHOISES) #multiple choise
     # vcm_status = MultiSelectField(max_length = 12, choices = status_vcm )
 
+
+
 class group (models.Model):
     group_code = models.CharField(primary_key = True, max_length = 100)
     group_name = models.CharField(max_length = 100)
     # group_leader = models.ForeignKey(member, on_delete=models.CASCADE)
     # group_center_code = models.ForeignKey(center,on_delete=models.CASCADE)
     collection_time = models.ForeignKey(collection_time,on_delete=models.CASCADE)
+class Role(models.Model):
+    role_name = models.CharField(max_length = 100, null = True)
+
+class User(models.Model):
+    role = models.ManyToManyField(Role,null = True)
+    mem_num = models.CharField(primary_key = True, default = '00000',max_length = 100)
+    first_name = models.CharField(max_length = 100, null = True)
+    middle_name = models.CharField(max_length = 100, null = True)
+    last_name = models.CharField(max_length = 100, null = True)
+    aadhar = models.CharField(max_length = 100, null = True)
+    pan = models.CharField(max_length = 100, null = True)
+    date_of_birth = models.DateField( null = True)
+    # marital_status = models.MultiSelectField()
+    mobile_registred = models.CharField(max_length = 100, null = True)
+    mobile_whatsapp = models.CharField(max_length = 100, null = True)
+    date_of_signup = models.DateField(null = True)
+    date_of_memstart = models.DateField( null = True)
+    # collection_frequency = models.MultiSelectField()
+    cot_balance = models.IntegerField( null = True)
+    cot_emi = models.IntegerField( null = True)
+    sa_balance = models.IntegerField( null = True)
+    user_group = models.ForeignKey(group,to_field= 'group_code', null = True,on_delete=models.CASCADE)
+    # fo = models.ForeignKey(fo,)
+    user_branch = models.ForeignKey(branch,to_field = 'branch_code', on_delete = models.CASCADE, null = True)
+    user_center = models.ForeignKey(center,to_field = 'center_code', on_delete =  models.CASCADE, null = True)
+    # user_status  = models.MultiSelectField()
+    # user_role = 
+    # community = 
+    date_of_food_distribution = models.DateField( null = True)
+
+
+
 
 class GLA(models.Model):
-    gla_branch = models.ForeignKey(branch , on_delete=models.CASCADE)
-    # borrower = models.ForeignKey(member , on_delete=models.CASCADE)
+    gla_branch = models.ForeignKey(branch, to_field = 'branch_name', on_delete=models.CASCADE)
+    borrower = models.ForeignKey(User, to_field = 'mem_num', default = 'mem_num', on_delete=models.CASCADE)
     cot_balance = models.DecimalField(max_digits=9, decimal_places=2)
     param_interest_rate_gl = models.IntegerField()
     param_interest_rate_glp = models.IntegerField()
@@ -113,32 +147,5 @@ class gold_lot(models.Model):
     tr_cancellation = models.DateField()
     # retrieval_outcome = multiple choice 
 
-class Role(models.Model):
-    role_name = models.CharField(max_length = 100, null = True)
 
-class User(models.Model):
-    role = models.ManyToManyField(Role,null = True)
-    mem_num = models.CharField(primary_key = True,max_length = 100)
-    first_name = models.CharField(max_length = 100, null = True)
-    middle_name = models.CharField(max_length = 100, null = True)
-    last_name = models.CharField(max_length = 100, null = True)
-    aadhar = models.CharField(max_length = 100, null = True)
-    pan = models.CharField(max_length = 100, null = True)
-    date_of_birth = models.DateField( null = True)
-    # marital_status = models.MultiSelectField()
-    mobile_registred = models.CharField(max_length = 100, null = True)
-    mobile_whatsapp = models.CharField(max_length = 100, null = True)
-    date_of_signup = models.DateField(null = True)
-    date_of_memstart = models.DateField( null = True)
-    # collection_frequency = models.MultiSelectField()
-    cot_balance = models.IntegerField( null = True)
-    cot_emi = models.IntegerField( null = True)
-    sa_balance = models.IntegerField( null = True)
-    user_group = models.ForeignKey(group,to_field= 'group_code', null = True,on_delete=models.CASCADE)
-    # fo = models.ForeignKey(fo,)
-    user_branch = models.ForeignKey(branch,to_field = 'branch_code', on_delete = models.CASCADE, null = True)
-    user_center = models.ForeignKey(center,to_field = 'center_code', on_delete =  models.CASCADE, null = True)
-    # user_status  = models.MultiSelectField()
-    # user_role = 
-    # community = 
-    date_of_food_distribution = models.DateField( null = True)
+
