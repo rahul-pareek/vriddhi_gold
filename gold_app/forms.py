54eec2481f71101bb2db8e0226ef8ae9e3eaffce
Rahul <pareek.rahul199@gmail.com>
2020-10-24 23:30:31 +0530

    forms & some updated_files

--- gold_app/forms.py
+++ gold_app/forms.py
@@ -1,6 +1,13 @@
 from django import forms 
 from .models import GLA
+
 class GLAForm(forms.ModelForm):
+    # def __init__(self,*args, **kwargs):
+    #     self.user_branch_id = kwargs.pop('user_branch_id')
+    #     self.fields['user_branch_id'].label = user_branch_id
+    #     super(GLAForm, self).__init__(*args, **kwargs)
+        
+
     class Meta: 
         model = GLA 
         fields = "__all__"
--- gold_app/views.py
+++ gold_app/views.py
@@ -5,11 +5,15 @@ from django.contrib.postgres.search import SearchVector
 
 def home_view(request): 
     context ={} 
-    form = GLAForm(request.POST or None, request.FILES or None) 
-    if form.is_valid(): 
-        form.save() 
-    context['form']= form 
-    return render(request, "home.html", context) 
+    # user_branch_id = request.GET.get('user_branch_id')
+    # mem_num = request.GET.get('mem_num')
+    form = GLAForm() 
+    if request.method == "POST":
+        form = GLAForm(request.POST)
+        if form.is_valid(): 
+            form.save() 
+    
+    return render(request, "home.html", locals()) 
 
 
 def show(request):
@@ -25,8 +29,9 @@ def add(request):
     search_vector = SearchVector('user_branch_id')
     if ('q' in request.GET) and request.GET.get('q').strip():
         query_string=request.GET.get('q')
-        seens = User.objects.filter(user_branch__icontains=request.GET.get('q')).values()    
-        print(seens)
+        seens = User.objects.filter(user_branch__branch_name__icontains=request.GET.get('q')).values()    
+        # print(seens)
+        # import  pdb; pdb.set_trace()
     else:
        seens=None
     return render(request,'./show_member.html',{'seens': seens})
